ee871a4639446b3a033713084d73171ed206bd62
njharlen <njharlen@broadinstitute.org>
2026-08-12 12:56:45 -0400

    bumped docker

--- update_docker.sh
+++ update_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker buildx build --platform=linux/amd64 -t gcr.io/broad-getzlab-fmhcrsparc/hcrseq:v0.9 .
-docker push gcr.io/broad-getzlab-fmhcrsparc/hcrseq:v0.9
+docker buildx build --platform=linux/amd64 -t gcr.io/broad-getzlab-fmhcrsparc/hcrseq:v1.0 .
+docker push gcr.io/broad-getzlab-fmhcrsparc/hcrseq:v1.0
 
